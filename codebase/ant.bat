3f202d3e59127c552601c3c3de5c96bec7b30439
Tim Pokorny <tim.pokorny@calytrix.com>
2012-08-12 23:17:49 +0800

    Windows ant batchfile launcher now points to the correct library directory to load the optionals.

--- codebase/ant.bat
+++ codebase/ant.bat
@@ -9,6 +9,6 @@ rem into the main distribution.
 
 set DEVELOPMENT_HOME=.
 set ANT_HOME=%DEVELOPMENT_HOME%\system\ant\apache-ant-1.8.4-patched
-set ANT_LIB=%DEVELOPMENT_HOME%\support\ant-optional\
+set ANT_LIB=%DEVELOPMENT_HOME%\system\ant-optional\
 
 "%ANT_HOME%\bin\ant.bat" %* -lib %ANT_LIB%
