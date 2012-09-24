9639d8e2f3c229d180adc36a4b59e5590f882eb0
Tim Pokorny <tim.pokorny@calytrix.com>
2012-09-24 11:10:34 +0800

    PORT-71: Split the LogicalTimeFactoryFactory static methods out into their own source file so that they can be included in the separate libfedtime dll as required by the standard.

--- codebase/src/cpp/ieee1516e/src/types/time/LogicalTimeFactory.cpp
+++ codebase/src/cpp/ieee1516e/src/types/time/LogicalTimeFactory.cpp
@@ -28,47 +28,4 @@ LogicalTimeFactory::~LogicalTimeFactory() throw()
 //                                     INSTANCE METHODS
 //------------------------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------------------
-//                                      STATIC METHODS
-//------------------------------------------------------------------------------------------
-/* 
- * The name is used to choose among several LogicalTimeFactories that might
- * be present in the fedtime library.  Each federation chooses its
- * implementation by passing the appropriate name to createFederationExecution.
- * If the supplied name is the empty string, a default LogicalTimeFactory is
- * returned.  If the supplied implementation name does not match any name
- * supported by the library, then a NULL pointer is returned.
- */
-std::auto_ptr<LogicalTimeFactory>
-LogicalTimeFactoryFactory::makeLogicalTimeFactory( const std::wstring& implementationName )
-{
-	return HLAlogicalTimeFactoryFactory::makeLogicalTimeFactory( implementationName );
-}
-
-/*
- * Provides a factory for the standard logical time types HLAfloat64Time
- * and HLAinteger64Time. The RTI reference time library's LogicalTimeFactoryFactory
- * should just forward requests to here.
- */
-std::auto_ptr<LogicalTimeFactory>
-HLAlogicalTimeFactoryFactory::makeLogicalTimeFactory( const std::wstring& implementationName )
-{
-	if( implementationName.compare(L"HLAfloat64TimeFactory") == 0 )
-	{
-		return auto_ptr<LogicalTimeFactory>( new HLAfloat64TimeFactory() );
-	}
-	else if( implementationName.compare(L"HLAinteger64TimeFactory") == 0 )
-	{
-		return auto_ptr<LogicalTimeFactory>( new HLAinteger64TimeFactory() );
-	}
-	else
-	{
-		wstringstream wss;
-		wss << "Unknown time implementation type [" << implementationName <<
-			"]: Must be HLAfloat64TimeFactory or HLAinteger64TimeFactory";
-		
-		throw InternalError( wss.str() );
-	}
-}
-
 IEEE1516E_NS_END
