e4bef79d69e2cc9adb966618088171948f5af205
Tim Pokorny <tim.pokorny@calytrix.com>
2013-03-25 14:08:25 +0800

    Fixing up header comments on all the tests for HLA-Evolved encoding helpers written by @michaelrfraser BECAUSE HE KILLED THE FORMATTING. The Shame.

--- codebase/src/java/test/hlaunit/hla1516e/types/encoding/HLAunicodeSringTest.java
+++ codebase/src/java/test/hlaunit/hla1516e/types/encoding/HLAunicodeSringTest.java
@@ -1,14 +1,16 @@
 /*
- * Copyright 2012 The Portico Project
- * 
- * This file is part of portico.
- * 
- * portico is free software; you can redistribute it and/or modify it under the terms of the
- * Common Developer and Distribution License (CDDL) as published by Sun Microsystems. For more
- * information see the LICENSE file.
- * 
- * Use of this software is strictly AT YOUR OWN RISK!!! If something bad happens you do not have
- * permission to come crying to me. (that goes for your lawyer as well)
+ *   Copyright 2012 The Portico Project
+ *
+ *   This file is part of portico.
+ *
+ *   portico is free software; you can redistribute it and/or modify
+ *   it under the terms of the Common Developer and Distribution License (CDDL) 
+ *   as published by Sun Microsystems. For more information see the LICENSE file.
+ *   
+ *   Use of this software is strictly AT YOUR OWN RISK!!!
+ *   If something bad happens you do not have permission to come crying to me.
+ *   (that goes for your lawyer as well)
+ *
  */
 package hlaunit.hla1516e.types.encoding;
 
