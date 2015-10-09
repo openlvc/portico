8e99f4dcb3335aeee34b4ef97e4d104be9a46bd4
Tim Pokorny <tim.pokorny@calytrix.com>
2015-10-09 12:30:26 +0800

    Add lancher batch file and bash script for the WAN router

--- codebase/resources/dist/common/bin/wanrouter.sh
+++ codebase/resources/dist/common/bin/wanrouter.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-java -Xmx4G -jar lib/portico.jar wanrouter $*
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PORTICO_HOME=$DIR/..
+$PORTICO_HOME/jre/bin/java -jar $PORTICO_HOME/lib/portico.jar wanrouter $*
 
