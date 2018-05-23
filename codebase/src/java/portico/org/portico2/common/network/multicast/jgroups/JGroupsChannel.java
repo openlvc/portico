b68268043121613bfeab023fd006775230e3f458
Tim Pokorny <tim.pokorny@calytrix.com>
2018-05-23 13:01:22 +0800

    Move JGroups classes into the multicast connection package

--- codebase/src/java/portico/org/portico2/common/network/multicast/jgroups/JGroupsChannel.java
+++ codebase/src/java/portico/org/portico2/common/network/multicast/jgroups/JGroupsChannel.java
@@ -12,7 +12,7 @@
  *   (that goes for your lawyer as well)
  *
  */
-package org.portico2.common.network.jgroups;
+package org.portico2.common.network.multicast.jgroups;
 
 import java.io.InputStream;
 import java.io.OutputStream;
